2046ca59d506e3f4409189c63610c0d157f58cc3
Gaspard Kemlin <gaspard.kemlin@enpc.fr>
2022-09-26 14:30:34 +0200

    shift hamiltonian

--- compare_shifted_sternheimer/compare_shifted_sternheimer.jl
+++ compare_shifted_sternheimer/compare_shifted_sternheimer.jl
@@ -3,59 +3,84 @@ using Dates
 using JSON
 using LinearAlgebra
 using ForwardDiff
-using TimerOutputs
+using JLD2
+
+using MPI
+disable_threading()
 
 include("./shifted_sternheimer.jl")
 
 function run()
+    function build_magnetic_moments(atoms::AbstractArray; magmoms...)
+        magmoms = Dict(magmoms)
+        map(atoms) do (element)
+            magmoms[element.symbol]
+        end
+    end
 
-    # set up model
-    a = 10.26
-    lattice = a / 2 * [[0 1 1.];
-                       [1 0 1.];
-                       [1 1 0.]]
-    Si = ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4"))
-    atoms     = [Si, Si]
-    positions = [ones(3)/8, -ones(3)/8]
-    kgrid = [1, 1, 1]
-    Ecut = 5
-    temperature = 0.0
+    function attach_pseudos(atoms::AbstractArray; pseudomap...)
+        pseudomap = Dict(pseudomap)
+
+        map(atoms) do (element)
+            pspfile = get(pseudomap, element.symbol, nothing)
+            ElementPsp(element.symbol, psp=load_psp(pspfile))
+        end
+    end
+
+    lattice   = load_lattice("AlFe2Mn_qe.in")
+    positions = load_positions("AlFe2Mn_qe.in")
+    atoms     = load_atoms("AlFe2Mn_qe.in")
+    atoms     = attach_pseudos(atoms, Mn="hgh/pbe/mn-q15.hgh",
+                               Fe="hgh/pbe/fe-q16.hgh",
+                               Al="hgh/pbe/al-q3.hgh")
+    magnetic_moments = build_magnetic_moments(atoms; Al=0.0, Fe=5.0, Mn=5.0)
+    if mpi_master()
+        println(atoms)
+        println(magnetic_moments)
+    end
+    smearing         = Smearing.Gaussian()
+    temperature      = 0.01
+    Ecut             = 45
+    kgrid            = [13, 13, 13]
+    supersampling    = 2.0
+    mixing           = KerkerMixing()
+    tol              = 1e-12
 
     model = model_PBE(lattice, atoms, positions;
-                      temperature, spin_polarization=:spinless)
+                      temperature, magnetic_moments, smearing)
     basis = PlaneWaveBasis(model; Ecut, kgrid)
-    tol = 1e-12
 
     # log and results files
+    proc_id  = MPI.Comm_rank(basis.comm_kpts)
     res_file = "SCF_results.json"
-    log_file_shifted = "sternheimer_log_shifted.json"
-    log_file         = "sternheimer_log.json"
+    log_file         = "sternheimer_log_proc$(proc_id).json"
+    log_file_noextra = "sternheimer_log_proc$(proc_id)_noextra.json"
 
     # print setup
-    println()
-    println("hostname        = $(read(`hostname`, String))")
-    println("started on      = $(Dates.now())")
-    println("julia threads   = $(Threads.nthreads())")
-    println("BLAS threads    = $(BLAS.get_num_threads())")
-    println()
-    println("a               = $(a)")
-    println()
-    println("temperature     = $(basis.model.temperature)")
-    println("smearing        = $(basis.model.smearing)")
-    println("lattice         = $(round.(basis.model.lattice, sigdigits=4))")
-    println("Ecut            = $(basis.Ecut)")
-    println("fft_size        = $(basis.fft_size)")
-    println("kgrid           = $(basis.kgrid)")
-    println("kshift          = $(basis.kshift)")
-    println("n_irreducible_k = $(sum(length, basis.krange_allprocs))")
-    println("n_bands         = $(DFTK.default_n_bands(basis.model))")
-    println("n_electrons     = $(basis.model.n_electrons)")
-    flush(stdout)
-    println("\n--------------------------------")
+    if mpi_master()
+        println()
+        println("hostname        = $(read(`hostname`, String))")
+        println("started on      = $(Dates.now())")
+        println("julia threads   = $(Threads.nthreads())")
+        println("BLAS threads    = $(BLAS.get_num_threads())")
+        println("MPI procs       = $(mpi_nprocs())")
+        println()
+        println("temperature     = $(basis.model.temperature)")
+        println("smearing        = $(basis.model.smearing)")
+        println("lattice         = $(round.(basis.model.lattice, sigdigits=4))")
+        println("Ecut            = $(basis.Ecut)")
+        println("fft_size        = $(basis.fft_size)")
+        println("kgrid           = $(basis.kgrid)")
+        println("kshift          = $(basis.kshift)")
+        println("n_irreducible_k = $(sum(length, basis.krange_allprocs))")
+        println("n_bands         = $(DFTK.default_n_bands(basis.model))")
+        println("n_electrons     = $(basis.model.n_electrons)")
+        flush(stdout)
+        println("\n--------------------------------")
+    end
 
     # results dictionnary
     res_dict = Dict(
-                    "a"            => a,
                     "temperature"  => basis.model.temperature,
                     "smearing"     => string(basis.model.smearing),
                     "lattice"      => basis.model.lattice,
@@ -65,6 +90,7 @@ function run()
                     "n_kirred"     => sum(length, basis.krange_allprocs),
                     "n_bands"      => DFTK.default_n_bands(basis.model),
                     "n_electr"     => basis.model.n_electrons,
+                    "mpi_n_procs"  => DFTK.mpi_nprocs(),
                     #
                     "energies"     => Float64[],
                     "ndiag"        => Int[],
@@ -84,40 +110,68 @@ function run()
     callback_SCF = ExtractCallback() ∘ DFTK.ScfDefaultCallback()
 
     # custom callback for sternheimer solver
-    log_dict_shifted = Dict{Int64, Array{Float64}}()
-    log_dict         = Dict{Int64, Array{Float64}}()
+    log_dict         = Dict{Tuple{Array{Float64}, Int64}, Dict{Int64, Array{Float64}}}()
+    log_dict_noextra = Dict{Tuple{Array{Float64}, Int64}, Dict{Int64, Array{Float64}}}()
     function callback_sternheimer!(dict)
         function callback(info)
-            dict[info.n] = info.ch[:resnorm]
+            coord   = info.kpoint.coordinate
+            spin    = info.kpoint.spin
+            kpt_id  = (coord, spin)
+            n       = info.n
+            ch      = info.ch
+
+            if haskey(dict, kpt_id)
+                dict[kpt_id][n] = ch[:resnorm]
+            else
+                dict[kpt_id] = Dict{Int64, Array{Float64}}(n => ch[:resnorm])
+            end
         end
     end
-
     # run SCF and sternheimer
-    scfres = self_consistent_field(basis; tol=tol, callback=callback_SCF)
+    DFTK.reset_timer!(DFTK.timer)
+    ρ0 = guess_density(basis, magnetic_moments)
+    scfres = DFTK.scf_potential_mixing(basis; tol, mixing, ρ=ρ0,
+                                       occupation_threshold=1e-8,
+                                       callback=callback_SCF)
+    if mpi_master()
+        println(DFTK.timer)
+    end
+    save_scfres("scfres.jld2", scfres);
     # generate appropriate right-hand-side from atomic displacements
-    R = [zeros(3), ones(3)]
+    R = [zeros(3) for pos in positions]
+    for iR in 1:length(R)
+        R[iR] = -ones(3) + 2*rand(3)
+    end
     function Vψ(ε)
         T = typeof(ε)
         pos = positions + ε*R
         modelV = Model(Matrix{T}(lattice), atoms, pos; model_name="potential",
-                       terms=[DFTK.AtomicLocal(),
-                              DFTK.AtomicNonlocal()])
+                       magnetic_moments,
+                       terms=[DFTK.AtomicLocal(), DFTK.AtomicNonlocal()])
         basisV = PlaneWaveBasis(modelV; Ecut, kgrid)
         jambon = Hamiltonian(basisV)
         jambon * Vector{Matrix{Complex{T}}}(scfres.ψ)
     end
     δVψ = ForwardDiff.derivative(Vψ, 0.0)
     # default setup with extra bands
-    println("\n--------------------------------")
-    println("apply_χ0 with extra bands")
+    if mpi_master()
+        println("\n--------------------------------")
+        println("apply_χ0 with extra bands")
+    end
     DFTK.reset_timer!(DFTK.timer)
     δψ1 = DFTK.apply_χ0_4P(scfres.ham, scfres.ψ, scfres.occupation, scfres.εF,
                            scfres.eigenvalues, δVψ; scfres.occupation_threshold,
                            callback=callback_sternheimer!(log_dict))
-    println(DFTK.timer)
+    if mpi_master()
+        println(DFTK.timer)
+        println("\n--------------------------------")
+        println("apply_χ0 without extra bands")
+    end
+    # no extra bands
+    DFTK.reset_timer!(DFTK.timer)
     println("\n--------------------------------")
     println("apply_χ0 with shilfted hamiltonian")
-    # setup without extra bands
+    # setup with the shifted Hamiltonian
     DFTK.reset_timer!(DFTK.timer)
     ψ_occ, occ_occ = DFTK.select_occupied_orbitals(basis,
                                                    scfres.ψ,
@@ -125,15 +179,26 @@ function run()
                                                    threshold=scfres.occupation_threshold)
     ε_occ = [scfres.eigenvalues[ik][1:size(ψk,2)] for (ik, ψk) in enumerate(ψ_occ)]
     δψ2 = apply_χ0_shifted(scfres.ham, ψ_occ, occ_occ, scfres.εF, ε_occ, δVψ;
-                           scfres.occupation_threshold,
+                           scfres.occupation_threshold, abstol=1e-12,
                            callback=callback_sternheimer!(log_dict_shifted))
     println(DFTK.timer)
 
     println("\n--------------------------------")
-    δρ1 = DFTK.compute_δρ(basis, scfres.ψ, δψ1, scfres.occupation)
-    δρ2 = DFTK.compute_δρ(basis, ψ_occ, δψ2, occ_occ)
+    δψ1, _ = DFTK.select_occupied_orbitals(basis,
+                                           δψ1,
+                                           scfres.occupation;
+                                           threshold=scfres.occupation_threshold)
+    δρ1 = DFTK.compute_δρ(basis, ψ_occ, δψ1, occ_occ)
+    δρ2 = DFTK.compute_δρ(basis, ψ_occ, δψ2, [ones(size(occk)) for occk in occ_occ])
+
     @show norm(δρ1 - δρ2)
-    @infiltrate
+    @show sum(δρ1)
+    @show sum(δρ2)
+
+    # make sure we have the same density response
+    @assert norm(δρ1 - δρ2) < 5e-8
+    @assert sum(δρ1) < 1e-12
+    @assert sum(δρ2) < 1e-12
 
     # write output files
     res_dict["eigenvalues"] = scfres.eigenvalues
--- compare_shifted_sternheimer/shifted_sternheimer.jl
+++ compare_shifted_sternheimer/shifted_sternheimer.jl
@@ -1,5 +1,5 @@
 # we implement here the solution of the sternheimer equation with a shifted
-# Hamiltonian as it is presented in eq (72) (and with same notations) of
+# Hamiltonian as it is presented in eq (72) of
 # S. Baroni, S. de Gironcoli, A. Dal Corso, and P. Giannozzi.
 # Phonons and related crystal properties from density-functional perturbation theory.
 # Reviews of Modern Physics, 73(2):515–562, 2001.
@@ -54,41 +54,44 @@ using IterativeSolvers
 
         # build operator Q (independant of n)
         Δ = 4 * temperature
-        A = Diagonal(εF .+ Δ .- εk)
+        A = Diagonal(max.(εF .+ Δ .- εk, 0))
         Q(ϕ) = ψk * A * (ψk' * ϕ)
 
         for n = 1:length(εk)
-            θFn    = filled_occ * Smearing.occupation(model.smearing, (εk[n]-εF) / temperature)
-            θFm(m) = filled_occ * Smearing.occupation(model.smearing, (εk[m]-εF) / temperature)
-            θnm(m) = Smearing.occupation(model.smearing, (εk[m]-εk[n]) / temperature)
-            θmn(m) = Smearing.occupation(model.smearing, (εk[n]-εk[m]) / temperature)
+            fn    = filled_occ * Smearing.occupation(model.smearing, (εk[n]-εF) / temperature)
+            fm(m) = filled_occ * Smearing.occupation(model.smearing, (εk[m]-εF) / temperature)
+            fnm(m) = Smearing.occupation(model.smearing, (εk[n]-εk[m]) / temperature)
+            fmn(m) = Smearing.occupation(model.smearing, (εk[m]-εk[n]) / temperature)
             ddiff = Smearing.occupation_divided_difference
             Rmn(m) = filled_occ * ddiff(model.smearing, εk[n], εk[m], εF, temperature)
 
             # build operator Pn (dependant of n)
-            β = Diagonal([θFn*θnm(m) + θFm(m)*θmn(m) + A[m,m]*Rmn(m)*θmn(m)
+            β = Diagonal([fn*fmn(m) + fm(m)*fnm(m) + A[m,m]*Rmn(m)*fnm(m)
                           for m in 1:length(εk)])
             Pn(ϕ) = ψk * β * (ψk' * ϕ)
 
             # build rhs
+            ψkn = ψk[:, n]
             δHψkn = δHψk[:,n]
-            rhs = - (θFn*δHψkn - Pn(δHψkn))
+            Q0(ϕ) = ϕ - ψkn * (ψkn' * ϕ)
+            rhs = - Q0(fn*δHψkn - Pn(δHψkn))
 
             # build and solve shifted sternheimer equation
             function shifted_ham(ϕ)
-                Hk*ϕ + Q(ϕ) - εk[n]*ϕ
+                Q0(Hk*ϕ + Q(ϕ) - εk[n]*ϕ)
             end
             precon = PreconditionerTPA(basis, kpoint)
             DFTK.precondprep!(precon, ψk[:, n])
             function ldiv!(x, y)
-                x .= precon \ y
+                x .= Q0(precon \ Q0(y))
             end
             J = LinearMap{eltype(ψk)}(shifted_ham, size(Hk, 1))
             δψkn, ch = cg(J, rhs; Pl=DFTK.FunctionPreconditioner(ldiv!),
                           abstol, reltol, verbose, log=true)
             info = (; basis=basis, kpoint=kpoint, ch=ch, n=n)
             callback(info)
-            δψk[:,n] = δψkn - Rmn(n) * δεF * ψk[:,n]
+            # add by hand the contribution onto ψkn
+            δψk[:,n] = δψkn + 1/2 * Rmn(n) * (dot(ψkn, δHψkn) - δεF) * ψkn
         end
         δψ[ik] = δψk
     end
